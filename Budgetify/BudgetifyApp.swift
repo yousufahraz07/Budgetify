a44f347a4f9215cd5b7721289a6e272897cf909b
yousufahraz07 <90855703+yousufahraz07@users.noreply.github.com>
2023-03-16 08:20:10 -0400

    Initial Commit

---
+++ Budgetify/BudgetifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetifyApp.swift
+//  Budgetify
+//
+//  Created by Ahraz  Yousuf on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct BudgetifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
